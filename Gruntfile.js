e41aa455ccbd89089f0a01dadcb6562f6a681620
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-03-30 15:08:23 -0300

    Done grunt task to concatenate all JS into one scripts file, and to concatenate all LESS into one CSS file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,41 @@
 module.exports = function (grunt) {
 
   grunt.initConfig({
+
     watch: {
       less: {
-        files: ['<%= jshint.files %>'],
+        files: ['public/css/**/*.less'],
         tasks: ['less']
+      },
+      js: {
+        files: ['public/js/prototype/**/*.js'],
+        tasks: ['concat']
       }
-    }
+    },
+
+    less: {
+      development: {
+        files: {
+          "public/css/styles.css": "public/css/**/*.less"
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: '\n\n// --------- Next import... ---------\n\n',
+      },
+      dist: {
+        src: ['public/js/prototype/**/*.js'],
+        dest: 'public/js/scripts.js'
+      }
+    },
+
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.registerTask('go', ['watch']);
 };
