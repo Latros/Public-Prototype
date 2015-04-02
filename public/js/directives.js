48a4cd6b82f2a8a778cb5752211e83b9f51b9451
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-04-02 16:47:59 -0300

    Almost done tweet card; and got *lots* of other stuff set up. A bunch of randomized dummy tweet JSON, miscellaneous style tweaks across the board, setting up the tweet card directive, and starting to plan for the layout algorithm I'm gonna have to write...

--- public/js/directives.js
+++ public/js/directives.js
@@ -17,56 +17,56 @@
   .directive('tweetCard', function () {
     return {
       scope: {
-        post: '=post'
+        tweet: '=tweet'
       },
       restrict: 'E',
-      templateUrl: '../html/partials/blog-card.partial.html',
-      link: function (scope, element, attributes) {}
-    };
-  })
-
-  .directive('instagramCard', function () {
-    return {
-      scope: {
-        post: '=post'
-      },
-      restrict: 'E',
-      templateUrl: '../html/partials/blog-card.partial.html',
-      link: function (scope, element, attributes) {}
-    };
-  })
-
-  .directive('redditCard', function () {
-    return {
-      scope: {
-        post: '=post'
-      },
-      restrict: 'E',
-      templateUrl: '../html/partials/blog-card.partial.html',
-      link: function (scope, element, attributes) {}
-    };
-  })
-
-  .directive('youtubeCard', function () {
-    return {
-      scope: {
-        post: '=post'
-      },
-      restrict: 'E',
-      templateUrl: '../html/partials/blog-card.partial.html',
-      link: function (scope, element, attributes) {}
-    };
-  })
-
-  .directive('facebookCard', function () {
-    return {
-      scope: {
-        post: '=post'
-      },
-      restrict: 'E',
-      templateUrl: '../html/partials/blog-card.partial.html',
+      templateUrl: '../html/partials/tweet-card.partial.html',
       link: function (scope, element, attributes) {}
     };
   });
+
+  // .directive('instagramCard', function () {
+  //   return {
+  //     scope: {
+  //       post: '=post'
+  //     },
+  //     restrict: 'E',
+  //     templateUrl: '../html/partials/blog-card.partial.html',
+  //     link: function (scope, element, attributes) {}
+  //   };
+  // })
+
+  // .directive('redditCard', function () {
+  //   return {
+  //     scope: {
+  //       post: '=post'
+  //     },
+  //     restrict: 'E',
+  //     templateUrl: '../html/partials/blog-card.partial.html',
+  //     link: function (scope, element, attributes) {}
+  //   };
+  // })
+
+  // .directive('youtubeCard', function () {
+  //   return {
+  //     scope: {
+  //       post: '=post'
+  //     },
+  //     restrict: 'E',
+  //     templateUrl: '../html/partials/blog-card.partial.html',
+  //     link: function (scope, element, attributes) {}
+  //   };
+  // })
+
+  // .directive('facebookCard', function () {
+  //   return {
+  //     scope: {
+  //       post: '=post'
+  //     },
+  //     restrict: 'E',
+  //     templateUrl: '../html/partials/blog-card.partial.html',
+  //     link: function (scope, element, attributes) {}
+  //   };
+  // });
 
 })();
