8bd84e2c4ffdebaed4afaa1b35d5df916ce70272
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-04-05 00:12:46 -0300

    Beginning layout engine work

--- public/js/directives.js
+++ public/js/directives.js
@@ -3,6 +3,34 @@
 
   angular.module('Prototype')
 
+  .directive('cardLayout', function () {
+    return {
+      scope: {
+        cards: '=cards'
+      },
+      restrict: 'E',
+      templateUrl: '../html/partials/card-layout.partial.html',
+      link: function (scope, element, attributes) {
+        var columns = {};
+
+        console.log(element[0].querySelector('.card-layout')
+          .offsetWidth);
+
+        var calculateColumns = function () {
+
+        };
+
+        var placeCards = function () {
+
+        };
+
+        var buildColumns = function () {
+
+        };
+      }
+    };
+  })
+
   .directive('newsCard', function () {
     return {
       scope: {
