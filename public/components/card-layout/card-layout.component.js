35d38759e7372528fb4a886d7f49a0c43bdf62a6
Sam Stiles <samuel.dev.stiles@gmail.com>
2015-04-14 15:31:57 -0300

    Problem requirements written out

--- public/components/card-layout/card-layout.component.js
+++ public/components/card-layout/card-layout.component.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('Prototype')
-    .directive('cardLayout', function ($compile, $timeout) {
+    .directive('cardLayout', function ($timeout) {
       return {
         scope: {
           cards: '=cards'
@@ -11,34 +11,10 @@
         restrict: 'E',
         templateUrl: 'components/card-layout/card-layout.template.html',
         link: function (scope, element, attributes) {
-          scope.columnWidth = 300;
-          scope.originalCards = _.clone(scope.cards);
-          scope.board = angular.element(element[0]);
-
-          scope.placeCard = function (card) {
-            var cardString = JSON.stringify(card);
-            var cardEl = $compile('<' + card.type + '-card data="' + encodeURIComponent(cardString) + '"></' + card.type + '-card>')(scope);
-            angular.element(scope.board[0])
-              .append(cardEl);
-          };
-
-          scope.render = function () {
-            console.log('Beginning render...');
-            scope.cards = _.clone(scope.originalCards);
-            scope.board[0].innerHTML = '';
-
-            var l = scope.cards.length;
-
-            for (var i = 0; i < l; i++) {
-              scope.placeCard(scope.cards[0]);
-              scope.cards.shift();
-            }
-          };
-
-          scope.render();
+          scope.columnWidth = 400;
 
           $timeout(function () {
-            var msnry = new Masonry(scope.board[0], {
+            var msnry = new Masonry(angular.element(element[0])[0], {
               itemSelector: '.eyesover-card',
               gutter: 30,
               columnWidth: scope.columnWidth
